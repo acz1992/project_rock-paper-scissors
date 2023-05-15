4d7a6c9d9b744f0a412dbc075352da134bf9dcb1
acz19 <acz1992@hotmail.co.uk>
2023-05-15 18:06:01 +0200

    complete playRound() and game() functions, test and make sure everything works

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+function getPlayerSelection() {
+	let selection = prompt(
+		"Enter your choice of weapon",
+		"Rock, Paper or Scissors"
+	);
+	selection =
+		selection.substring(0, 1).toUpperCase() +
+		selection.slice(1).toLowerCase();
+	if (selection === "Rock" || "Paper" || "Scissors") {
+		return selection;
+	} else {
+		console.log(
+			"Invalid input, please select from either Rock, Paper or Scissors" &&
+				prompt("Enter your choice of weapon", "Rock, Paper or Scissors")
+		);
+	}
+}
+
 function getComputerChoice() {
 	let compNum = Math.floor(Math.random() * (3 - 1 + 1) + 1);
 	if (compNum === 1) {
@@ -36,3 +54,37 @@ function playRound(playerSelection, computerSelection) {
 		return "You win! Scissors beats Paper";
 	}
 }
+
+function game() {
+	let playerScore = 0;
+	let computerScore = 0;
+	for (let count = 0; count < 5; count++) {
+		let playerSelection = getPlayerSelection();
+		let computerSelection = getComputerChoice();
+		let roundResult = playRound(playerSelection, computerSelection);
+		console.log(`Round ${count + 1}: ${roundResult}`);
+		if (roundResult.includes("win")) {
+			playerScore++;
+		} else if (roundResult.includes("lose")) {
+			computerScore++;
+		}
+		console.log(`Player Score: ${playerScore}`);
+		console.log(`Computer Score: ${computerScore}`);
+	}
+
+	if (playerScore > computerScore) {
+		console.log(
+			`Final Score - Player: ${playerScore}, Computer: ${computerScore} \nCongratulations, you win!`
+		);
+	} else if (playerScore < computerScore) {
+		console.log(
+			`Final Score - Computer: ${computerScore}, Player: ${playerScore} \nAw shucks, you lose!`
+		);
+	} else {
+		console.log(
+			`Final Score - Player: ${playerScore}, Computer: ${computerScore} \nAh, annoyingly it is a tie!`
+		);
+	}
+}
+
+game();
